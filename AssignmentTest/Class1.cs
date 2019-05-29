11c07292a7ea90392907d658a4c4469c760180f0
admin <admin@DESKTOP-N6SU4MK>
2019-05-28 21:31:02 -0400

    Containing completed unit tests.

--- AssignmentTest/Class1.cs
+++ AssignmentTest/Class1.cs
@@ -3,64 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 using Assignment;
+using NUnit.Framework;
 
-namespace AssignmentTest
+namespace PROG8170_Assignment1_Test
 {
-    public class Rectangle
+    [TestFixture]
+    class PROG8170_Assignment1_Test
     {
-        private int length;
-        private int width;
-
-
-        public Rectangle()
+        [Test]
+        public void GetLength_Input25_Returns25()
         {
-            length = 1;
-            width = 1;
+
+            //Arrange
+            int length = 25;
+            int width = 13;
+
+            int expectedResult = length;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.GetLength();
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetWidth_Input33_Returns33()
+        {
+
+            //Arrange
+            int length = 13;
+            int width = 40;
+
+            int expectedResult = width;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.GetWidth();
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void SetLength_Input125_Returns125()
+        {
+
+            //Arrange
+            int length = 125;
+            int width = 18;
+
+            int expectedResult = length;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.SetLength(length);
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void Setwidth_Input35_Returns35()
+        {
+
+            //Arrange
+            int length = 35;
+            int width = 18;
+
+            int expectedResult = width;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.SetWidth(width);
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetPerimeter_Input_Length27And_Width27_Returns60()
+        {
+
+            //Arrange
+            int length = 27;
+            int width = 11;
+
+            int expectedResult = 2 * (length + width);
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.GetPerimeter();
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetArea_Input_Length7And_Width7_Returns49()
+        {
+
+            //Arrange
+            int length = 7;
+            int width = 7;
+
+            int expectedResult = (length * width);
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.GetArea();
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
-        public Rectangle(int length, int width)
+        static void Main(string[] args)
         {
-            this.length = length;
-            this.width = width;
-        }
 
-        public int GetLength()
-        {
-            return length;
-        }
-
-
-        public int SetLength(int length)
-        {
-            this.length = length;
-            return this.length;
-        }
-
-
-        public int GetWidth()
-        {
-            return width;
-        }
-
-
-        public int SetWidth(int width)
-        {
-            this.width = width;
-            return this.width;
-        }
-
-        public int GetPerimeter()
-        {
-            return (2 * (length + width));
-        }
-
-        public int GetArea()
-        {
-            return (length * width);
         }
 
     }
 }
-
